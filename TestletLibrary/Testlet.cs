1d81ae7201d4775bc5fc4f5b1daf586e3ac5efc6
ARCADIA\vladimir.skachkov <vladimir.skachkov@arcadia.spb.ru>
2021-11-24 20:07:02 +0300

    Initial commit.

---
+++ TestletLibrary/Testlet.cs
@@ -0,0 +1,39 @@
+﻿namespace TestletLibrary
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Testlet
+    {
+        public string TestletId;
+
+        private List<Item> Items;
+
+        public Testlet(string testletId, List<Item> items)
+        {
+            TestletId = testletId;
+            Items = items;
+        }
+
+        public List<Item> Randomize()
+        {
+            throw new NotImplementedException();
+            //Items private collection has 6 Operational and 4 Pretest Items. Randomize the order of these items as per the requirement (with TDD)
+            //The assignment will be reviewed on the basis of – Tests written first, Correct logic, Well structured & clean readable code.
+        }
+    }
+
+    public class Item
+    {
+        public string ItemId;
+
+        public ItemTypeEnum ItemType;
+    }
+
+    public enum ItemTypeEnum
+    {
+        Pretest = 0,
+
+        Operational = 1
+    }
+}
