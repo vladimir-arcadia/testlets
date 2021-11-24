9485717d6f1c385b0e28eb093ca5fcac76f3fe40
ARCADIA\vladimir.skachkov <vladimir.skachkov@arcadia.spb.ru>
2021-11-24 20:20:56 +0300

    Add project for tests.

---
+++ TestletLibrary.Tests/TestletTests.cs
@@ -0,0 +1,18 @@
+namespace TestletLibrary.Tests
+{
+    using Xunit;
+
+    public class TestletTests
+    {
+        [Fact]
+        public void Constructor_InvokedWithValidArguments_CreateInstance()
+        {
+            // TODO: Implement
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
