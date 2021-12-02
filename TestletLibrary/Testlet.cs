b4a3986a444c3bf3e44c413a75dea7b0cd40e9ad
ARCADIA\vladimir.skachkov <vladimir.skachkov@arcadia.spb.ru>
2021-12-02 19:01:51 +0300

    Remove double randomization of Pretest items. Fix code style.

--- TestletLibrary/Testlet.cs
+++ TestletLibrary/Testlet.cs
@@ -9,14 +9,14 @@
 
         public string TestletId;
 
-        private List<Item> Items;
+        private readonly List<Item> items;
 
-        private IRandomizer rnd;
+        private readonly IRandomizer rnd;
 
         public Testlet(string testletId, List<Item> items, IRandomizer rnd)
         {
-            TestletId = testletId;
-            Items = items;
+            this.TestletId = testletId;
+            this.items = items;
 
             // When we use some concrete implementation of the randomizer we can not control test environment.
             // To prevent occasional test fails we should delegate randomization to something that we can control.
@@ -25,18 +25,11 @@
 
         public List<Item> Randomize()
         {
-            var pretest = this.Items
-                .Where(item => item.ItemType == ItemTypeEnum.Pretest)
-                .OrderBy(item => this.rnd.NextInt()).ToList(); // Randomizing first two of the Pretests.
-
-            var ending = new List<Item>();
-            ending.AddRange(pretest.Skip(LeadingPretestNumber));
-            ending.AddRange(this.Items.Where(item => item.ItemType == ItemTypeEnum.Operational));
-
+            var randomized = this.items.OrderBy(item => this.rnd.NextInt()).ToList();
 
             var result = new List<Item>();
-            result.AddRange(pretest.Take(LeadingPretestNumber));
-            result.AddRange(ending.OrderBy(item => this.rnd.NextInt())); // Mixing the rest of Pretest and all the operational.
+            result.AddRange(randomized.Where(item => item.ItemType == ItemTypeEnum.Pretest).Take(LeadingPretestNumber));
+            result.AddRange(randomized.Where(item => !result.Contains(item)));
             return result;
         }
     }
